9bbbf37eddb04a1ae9a152360a898603dcc5e6ae
Pedro-relax <77356797+Pedro-relax@users.noreply.github.com>
2021-11-01 20:49:08 -0500

    working on javascript

--- js/commands.js
+++ js/commands.js
@@ -1,2 +1,9 @@
-
+	document.getElementById('sub').addEventListener('click',
+	function () {
+		var f = document.getElementById('fn').value;
+		var l = document.getElementById('ln').value;
+		if (isNaN(f)) {
+			alert("No numbers in here");
+		}
+	});
 		
