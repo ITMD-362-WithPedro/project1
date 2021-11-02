c39a99e543a31a2063e73ef274c105157b7e6347
Pedro-relax <77356797+Pedro-relax@users.noreply.github.com>
2021-11-01 20:56:12 -0500

    checking javascript

--- js/commands.js
+++ js/commands.js
@@ -1,9 +1,14 @@
-	document.getElementById('sub').addEventListener('click',
-	function () {
+	document.addEventListener('DOMContentLoaded',function(){
+	console.log ("It loaded");
+	document.getElementById('sub').addEventListener('click', function () {
 		var f = document.getElementById('fn').value;
 		var l = document.getElementById('ln').value;
 		if (isNaN(f)) {
 			alert("No numbers in here");
 		}
+		else if ( f === "") {
+			alert("Enter Name Please");
+		}
+	});
 	});
 		
